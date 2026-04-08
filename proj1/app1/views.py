c8d15e5343344bb8935a827fc92ef5fd08c657ba
user1 <leslie.cruz02@utrgv.edu>
2026-04-08 15:43:07 -0500

    fixed

--- proj1/app1/views.py
+++ proj1/app1/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'stellarproj1/index.html')
+    return render(request, 'proj1/index.html')
 
 def generic(request):
-    return render(request, 'stellarproj1/generic.html')
+    return render(request, 'proj1/generic.html')
 
 def elements(request):
-    return render(request, 'stellarproj1/elements.html')
+    return render(request, 'proj1/elements.html')
